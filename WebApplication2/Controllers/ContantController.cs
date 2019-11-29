0c9a423c5415a3b2b0070c3e7f09ade2264ca627
Lredstar <redstarnice@163.com>
2019-11-29 15:23:26 +0800

    000

---
+++ WebApplication2/Controllers/ContantController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ContantController : Controller
+    {
+        public IActionResult Index()
+        {
+            var contants = new List<Contant>();
+            for (int i = 1; i < 11; i++)
+            {
+                contants.Add(new Contant { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            }
+            return View(new ContantViewModel { Contants=contants});
+        }
+    }
+}
